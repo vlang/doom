429f943395341d9be760ca5759e6b6faa896ffc6
Alexander Medvednikov <alexander@vlang.io>
2022-06-16 20:32:42 +0300

    build all

---
+++ chocolate-doom/src/doom/build_all.sh
@@ -0,0 +1,7 @@
+cc -o doomv ~/code/doom/chocolate-doom/src/CMakeFiles/chocolate-doom.dir/*.o \
+  ~/code/doom/chocolate-doom/textscreen/CMakeFiles/textscreen.dir/*.o \
+  ~/code/doom/chocolate-doom/pcsound/CMakeFiles/pcsound.dir/*.o \
+  ~/code/doom/chocolate-doom/opl/CMakeFiles/opl.dir/*.o \
+  ~/code/doom/chocolate-doom/src/doom/c2v_out.dir/doom.o \
+  -L /opt/homebrew/Cellar/sdl2/2.0.14_1/lib -L /opt/homebrew/Cellar/sdl2_mixer/2.0.4_1/lib -L /opt/homebrew/Cellar/sdl2_net/2.0.1/lib -L /opt/homebrew/Cellar/libpng/1.6.37/lib \
+  -lsdl2 -lsdl2_mixer -lsdl2_net -lpng
