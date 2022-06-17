46dd1a8a6cec180b189dfc3601f6dc0cb01b0b4e
Alexander Medvednikov <alexander@vlang.io>
2022-06-17 16:00:22 +0300

    build sh fix

--- chocolate-doom/src/doom/build_doom_o.sh
+++ chocolate-doom/src/doom/build_doom_o.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-v -o c2v_out.dir/doom.c -is_o -gc none -showcc -cg -cflags "-w" -w -translated c2v_out.dir/
+v -o /tmp/doom.c -is_o -gc none -showcc -cg -cflags "-w" -w -translated c2v_out.dir/
 
-cc -c -w -std=c99 -D_DEFAULT_SOURCE -g -o c2v_out.dir/doom.o -x objective-c c2v_out.dir/doom.c  -x none -mmacosx-version-min=10.7 -rdynamic
+cc -c -w -std=c99 -D_DEFAULT_SOURCE -g -o c2v_out.dir/doom.o -x objective-c /tmp/doom.c  -x none -mmacosx-version-min=10.7 -rdynamic
