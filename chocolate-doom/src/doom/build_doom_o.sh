5e2eb59ec7f5c1c6f36b378ba76fa504706ffed7
Delyan Angelov <delian66@gmail.com>
2022-06-17 12:03:04 +0300

    change build scripts to include -lsamplerate and use sdl2-config, in order to not rely on hardcoded paths

--- chocolate-doom/src/doom/build_doom_o.sh
+++ chocolate-doom/src/doom/build_doom_o.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-v -o doom.c -is_o -gc none -showcc -cg -cflags "-w" -w -translated c2v_out.dir
+v -o c2v_out.dir/doom.c -is_o -gc none -showcc -cg -cflags "-w" -w -translated c2v_out.dir/
 
-cc -c -w -std=c99 -D_DEFAULT_SOURCE -g -o doom.o -x objective-c "doom.c"  -x none -mmacosx-version-min=10.7 -rdynamic
+cc -c -w -std=c99 -D_DEFAULT_SOURCE -g -o c2v_out.dir/doom.o -x objective-c c2v_out.dir/doom.c  -x none -mmacosx-version-min=10.7 -rdynamic
