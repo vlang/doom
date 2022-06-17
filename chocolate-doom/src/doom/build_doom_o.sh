231c3b5118a273cf8074ca56fd5a4e9c2d0031f0
Delyan Angelov <delian66@gmail.com>
2022-06-17 11:14:18 +0300

    add a shebang line to build_doom_o.sh

--- chocolate-doom/src/doom/build_doom_o.sh
+++ chocolate-doom/src/doom/build_doom_o.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
 v -o doom.c -is_o -gc none -showcc -cg -cflags "-w" -w -translated . || exit
 
 cc -c -w -std=c99 -D_DEFAULT_SOURCE -g -o doom.o -x objective-c "doom.c"  -x none -mmacosx-version-min=10.7 -rdynamic
