e6d58ab9da79c6fd1117b7b43bb17f5c8b9ee802
Delyan Angelov <delian66@gmail.com>
2022-06-18 21:30:34 +0300

    fix anims static initialisation in wi_stuff.c (fix end level segfault)

--- chocolate-doom/src/doom/wi_stuff.c
+++ chocolate-doom/src/doom/wi_stuff.c
@@ -271,14 +271,11 @@ static int NUMANIMS[NUMEPISODES] =
     6,//arrlen(epsd2animinfo),
 };
 
-static anim_t* anims[NUMEPISODES];
-/*
-{
-    epsd0animinfo,
-    epsd1animinfo,
-    epsd2animinfo
+static anim_t* anims[NUMEPISODES] = {
+    &epsd0animinfo,
+    &epsd1animinfo,
+    &epsd2animinfo,
 };
-*/
 
 
 //
@@ -524,11 +521,6 @@ WI_drawOnLnode
 
 void WI_initAnimatedBack(void)
 {
-
-anims[0] = epsd0animinfo;
-    anims[1]=epsd1animinfo;
-    anims[2]=epsd2animinfo;
-
     int		i;
     anim_t*	a;
 
