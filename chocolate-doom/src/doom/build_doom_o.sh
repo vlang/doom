4b2ef4f3e2a19f75e3f42c2a5b81ae18a55ec10e
Alexander Medvednikov <alexander@vlang.io>
2022-06-19 20:51:58 +0300

    simplify build_doom_o.sh

--- chocolate-doom/src/doom/build_doom_o.sh
+++ chocolate-doom/src/doom/build_doom_o.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-v -o /tmp/doom.c -is_o -gc none -showcc -cg -cflags "-w" -w -translated c2v_out.dir/
-
-cc -c -w -std=c99 -D_DEFAULT_SOURCE -g -o c2v_out.dir/doom.o -x objective-c /tmp/doom.c  -x none -mmacosx-version-min=10.7 -rdynamic
+v -o c2v_out.dir/doom.o -w -translated c2v_out.dir/
