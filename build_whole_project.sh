af693db17cc76873e86b562bc661d83338ee35ea
Alexander Medvednikov <alexander@vlang.io>
2022-06-20 18:07:16 +0300

    c2v_out.dir => doom_v

--- build_whole_project.sh
+++ build_whole_project.sh
@@ -9,13 +9,13 @@ if [ -z ${WAD_FILE+x} ]; then export WAD_FILE=~/Downloads/DOOM1.WAD; fi
 
 cd $DOOM_FOLDER/chocolate-doom ;
 git clean -xf ;
-rm -rf src/doom/c2v_out.dir/ ;
+rm -rf src/doom/doom_v/ ;
 cmake -DCMAKE_BUILD_TYPE=Debug . ;
 make chocolate-doom ;
 
 v translate src/doom ;
 
-cat << EOF > src/doom/c2v_out.dir/vdoom_patch_linux.v
+cat << EOF > src/doom/doom_v/vdoom_patch_linux.v
 module main
 const _isspace = 8192
 type U16 = u16
@@ -25,14 +25,14 @@ EOF
 
 cd $DOOM_FOLDER/chocolate-doom/src/doom ;
 ## compile the produced V source code to an .o file that can be linked to the rest:
-v -cc $CC -o c2v_out.dir/doom.o -w -translated c2v_out.dir/
+v -cc $CC -o doom_v/doom.o -w -translated doom_v/
 
 $CC -o doomv \
   $DOOM_FOLDER/chocolate-doom/src/CMakeFiles/chocolate-doom.dir/*.o \
   $DOOM_FOLDER/chocolate-doom/textscreen/CMakeFiles/textscreen.dir/*.o \
   $DOOM_FOLDER/chocolate-doom/pcsound/CMakeFiles/pcsound.dir/*.o \
   $DOOM_FOLDER/chocolate-doom/opl/CMakeFiles/opl.dir/*.o \
-  $DOOM_FOLDER/chocolate-doom/src/doom/c2v_out.dir/doom.o \
+  $DOOM_FOLDER/chocolate-doom/src/doom/doom_v/doom.o \
   $(sdl2-config --libs) -lSDL2_mixer -lSDL2_net -lpng -lsamplerate -lm
 
 cp $WAD_FILE .
